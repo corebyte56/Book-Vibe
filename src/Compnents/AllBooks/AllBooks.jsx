1563db58378b0d75dfa300ba791dd16babe6741d
shahariar ahammad fahim <fahim789m@gmail.com>
2026-04-11 04:13:20 +0600

    feat: install lucide-react for icons

--- src/Compnents/AllBooks/AllBooks.jsx
+++ src/Compnents/AllBooks/AllBooks.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const AllBooks = () => {
+const [showBookData, setShowBookData] = useState([]);
+
+  // Fetch data from bookData.json
+  const handleData = async () => {
+    try {
+      const response = await fetch("/booksData.json");
+      const data = await response.json();
+      setShowBookData(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    handleData();
+  }, []);
+
   return (
     <div className='md:py-20 py-10'>
       <h1 className='text-5xl font-bold'>Books</h1>
+      {/* cards */}
+
+      <div>
+        {
+        showBookData.map((books => (
+            <div>
+                <p>{books.author}</p>
+            </div>
+        )))
+      }
+      </div>
     </div>
   )
 }
