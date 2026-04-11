a4fed1424f00cd8a16971493e9c7d4b9d0f085af
shahariar ahammad fahim <fahim789m@gmail.com>
2026-04-12 03:36:02 +0600

    feat: implement react-tabs for separating readlist and wishlist UI

--- src/Pages/Books/Books.jsx
+++ src/Pages/Books/Books.jsx
@@ -2,24 +2,41 @@ import React, { useContext } from "react";
 import { bookContext } from "../../Context/context";
 import WishList from "../../Ui/WishList/WishList";
 import ReadList from "../../Ui/ReadList/ReadList";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 const Books = () => {
   //   context
   const { addToRead, addToWishList } = useContext(bookContext);
 
   return (
-    // Read List 
+    // Read List
     <div className="container mx-auto px-20">
-      {addToRead.map((readBook) => (
-        <ReadList readBook = {readBook} />
-      ))}
+      <Tabs>
+        <TabList>
+          <Tab>Read List</Tab>
+          <Tab>Wish List</Tab>
+        </TabList>
+
+        {/* Read List Tab */}
+        <TabPanel>
+          <div>
+            {addToRead.map((readBook) => (
+              <ReadList readBook={readBook} />
+            ))}
+          </div>
+        </TabPanel>
 
 
-      {
-        addToWishList.map((wishBook) => (
-        <WishList wishBook = {wishBook} />
-      ))
-      }
+        {/* Wish List Tab */}
+        <TabPanel>
+          <div>
+            {addToWishList.map((wishBook) => (
+              <WishList wishBook={wishBook} />
+            ))}
+          </div>
+        </TabPanel>
+      </Tabs>
     </div>
   );
 };
